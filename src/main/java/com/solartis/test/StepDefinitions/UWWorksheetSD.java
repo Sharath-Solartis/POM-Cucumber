7a188037832073fb1648c83ced0cbbbf12d26c5b
VigneshKumar-Solartis <vigneshkumar_p@solartis.net>
2018-03-12 17:30:45 +0530

    commit

--- src/main/java/com/solartis/test/StepDefinitions/UWWorksheetSD.java
+++ src/main/java/com/solartis/test/StepDefinitions/UWWorksheetSD.java
@@ -2,6 +2,7 @@ package com.solartis.test.StepDefinitions;
 
 import org.apache.log4j.Logger;
 
+import com.solartis.test.Pages.LoginPage;
 import com.solartis.test.Pages.UWWorksheetPage;
 
 import cucumber.api.java.en.Then;
@@ -12,9 +13,11 @@ public class UWWorksheetSD {
 	public void checks_if_the_base_wind_rate_is(String base_wind_rate) throws Throwable 
 	{
 		UWWorksheetPage uwpage = new UWWorksheetPage();
-		System.out.println("UWsheet compare");
-		//assertEquals(base_wind_rate,uwpage.BaseRateCheck());
-		uwpage.BaseRateCheck(base_wind_rate);
-		logger.info("Reading Underwriter Worksheet Factors");
+        LoginPage lp = new LoginPage();
+        System.out.println("UWsheet compare");
+        //assertEquals(base_wind_rate,uwpage.BaseRateCheck());
+        uwpage.BaseRateCheck(base_wind_rate);
+        lp.deinitialization();
+        logger.info("Reading Underwriter Worksheet Factors");
     }
 }
